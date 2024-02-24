c4053ff30648cd9c6d972fd80c7cd2656a9c2e66
Joseph-MHY <huamaniyallij@gmail.com>
2024-02-23 23:49:02 -0500

    probando pull

---
+++ app/src/main/java/pe/edu/idat/ec1_tito/NuevoArchivoPULL.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.ec1_tito
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevoArchivoPULL : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo_archivo_pull)
+    }
+}
